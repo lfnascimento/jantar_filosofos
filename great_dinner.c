5f40c6d0b52600b416c5d64ee384396c60793008
Luis Fernando Nascimento <nascimento.lsfo@gmail.com>
2015-06-13 20:29:20 -0300

    Renomeacao de funcoes e variaveis

--- great_dinner.c
+++ great_dinner.c
@@ -3,19 +3,19 @@
 #include<pthread.h>
   
 #define N 5
-#define THINKING 0
-#define HUNGRY 1
-#define EATING 2
-#define LEFT (ph_num+4)%N
-#define RIGHT (ph_num+1)%N
+#define PENSANDO 0
+#define FAMINTO 1
+#define COMENDO 2
+#define ESQUERDA (ph_num+4)%N
+#define DIREITA (ph_num+1)%N
   
 sem_t mutex;
 sem_t S[N];
   
-void * philospher(void *num);
+void * filosofo(void *num);
 void take_fork(int);
 void put_fork(int);
-void test(int);
+void tentaComer(int);
   
 int state[N];
 int phil_num[N]={0,1,2,3,4};
@@ -29,21 +29,21 @@ int main()
         sem_init(&S[i],0,0);
     for(i=0;i<N;i++)
     {
-        pthread_create(&thread_id[i],NULL,philospher,&phil_num[i]);
+        pthread_create(&thread_id[i],NULL,filosofo,&phil_num[i]);
 
     }
     for(i=0;i<N;i++)
         pthread_join(thread_id[i],NULL);
 }
   
-void *philospher(void *num)
+void *filosofo(void *num)
 {
     while(1)
     {
         int *i = num;
-        printf("Philosopher %d is thinking\n",i+1);
+        printf("Filosofo %d esta pensando\n",*i+1);
         take_fork(*i);
-        sleep(0);
+        //sleep(0);
         put_fork(*i);
     }
 }
@@ -51,22 +51,22 @@ void *philospher(void *num)
 void take_fork(int ph_num)
 {
     sem_wait(&mutex);
-    state[ph_num] = HUNGRY;
-    printf("Philosopher %d is Hungry\n",ph_num+1);
-    test(ph_num);
+    state[ph_num] = FAMINTO;
+    printf("Filosofo %d esta faminto\n",ph_num+1);
+    tentaComer(ph_num);
     sem_post(&mutex);
     sem_wait(&S[ph_num]);
     sleep(1);
 }
   
-void test(int ph_num)
+void tentaComer(int ph_num)
 {
-    if (state[ph_num] == HUNGRY && state[LEFT] != EATING && state[RIGHT] != EATING)
+    if (state[ph_num] == FAMINTO && state[ESQUERDA] != COMENDO && state[DIREITA] != COMENDO)
     {
-        state[ph_num] = EATING;
+        state[ph_num] = COMENDO;
         sleep(2);
-        printf("Philosopher %d takes fork %d and %d\n",ph_num+1,LEFT+1,ph_num+1);
-        printf("Philosopher %d is Eating\n",ph_num+1);
+        printf("Filosofo %d pega garfo %d e %d\n",ph_num+1,ESQUERDA+1,ph_num+1);
+        printf("Filosofo %d esta comendo\n",ph_num+1);
         sem_post(&S[ph_num]);
     }
 }
@@ -74,10 +74,10 @@ void test(int ph_num)
 void put_fork(int ph_num)
 {
     sem_wait(&mutex);
-    state[ph_num] = THINKING;
-    printf("Philosopher %d putting fork %d and %d down\n",ph_num+1,LEFT+1,ph_num+1);
-    printf("Philosopher %d is thinking\n",ph_num+1);
-    test(LEFT);
-    test(RIGHT);
+    state[ph_num] = PENSANDO;
+    printf("Filosofo %d coloca o garfo %d and %d na mesa\n",ph_num+1,ESQUERDA+1,ph_num+1);
+    printf("Filosfo %d esta pensando\n",ph_num+1);
+    tentaComer(ESQUERDA);
+    tentaComer(DIREITA);
     sem_post(&mutex);
 }
